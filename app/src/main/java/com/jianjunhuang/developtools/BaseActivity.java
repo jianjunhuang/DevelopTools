e346712c4cc9c5bf66473a67c3451621863ba6d4
JianjunHuang <903732218@qq.com>
2017-02-10 10:41:12 +0800

    Initial commit

---
+++ app/src/main/java/com/jianjunhuang/developtools/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.jianjunhuang.developtools;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author jianjunhuang.me@foxmail.com
+ * @since 2017/2/10.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+}
